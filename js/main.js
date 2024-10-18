b4d57515d2dd5808cd74dbe5e7e6a3256bc8d854
Navin <navin20604@gmail.com>
2024-10-17 23:40:11 -0400

    Building Navbar

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
+// Collapsibles
+const collapsibles = document.querySelectorAll(".collapsible");
+collapsibles.forEach((item) =>
+  item.addEventListener("click", function () {
+    this.classList.toggle("collapsible--expanded");
+  })
+);
+
+// skills
 const skill = Array.from(document.querySelectorAll(".skill"));
 skill.forEach((item) => {
   const skillPercent = item.children[1].children[1].textContent;
