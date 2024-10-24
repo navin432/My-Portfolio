281fa1301b9a5ffa23e0609365e5d13d7f725ced
Navin <navin20604@gmail.com>
2024-10-23 22:58:36 -0400

    Animating Skills Section

--- js/main.js
+++ js/main.js
@@ -31,10 +31,13 @@ skill.forEach((item) => {
   skillBackground.style.width = skillPercent;
 });
 
-// Higlighting Active Link
+// Higlighting Active Link and Animating The Skills section
 document.addEventListener("DOMContentLoaded", function () {
   const sections = document.querySelectorAll("section");
   const navLinks = document.querySelectorAll(".nav__list .list__item a");
+  const skillLevels = document.querySelectorAll(".skill__level");
+  const skillBars = document.querySelectorAll(".skill__line--content");
+  let skillsAnimated = false; // To ensure the animation runs only once
 
   const options = {
     root: null,
@@ -55,6 +58,27 @@ document.addEventListener("DOMContentLoaded", function () {
         );
         if (activeLink) {
           activeLink.parentElement.classList.add("active");
+        }
+
+        if (activeSectionId === "skills" && !skillsAnimated) {
+          skillLevels.forEach((skill, index) => {
+            const finalValue = skill.getAttribute("data-skill");
+            let currentValue = 0;
+
+            const increment = setInterval(() => {
+              skill.textContent = `${currentValue}%`;
+
+              if (currentValue >= finalValue) {
+                clearInterval(increment);
+              } else {
+                currentValue++;
+              }
+            }, 20);
+
+            skillBars[index].style.width = `${finalValue}%`;
+          });
+
+          skillsAnimated = true;
         }
       }
     });
