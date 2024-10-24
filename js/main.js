50b80f00d255dd65d6ebab105c5f7c9ff3c98fc0
Navin <navin20604@gmail.com>
2024-10-23 22:45:30 -0400

    Highlighting Active Section

--- js/main.js
+++ js/main.js
@@ -30,3 +30,37 @@ skill.forEach((item) => {
   const skillBackground = item.children[0].children[0];
   skillBackground.style.width = skillPercent;
 });
+
+// Higlighting Active Link
+document.addEventListener("DOMContentLoaded", function () {
+  const sections = document.querySelectorAll("section");
+  const navLinks = document.querySelectorAll(".nav__list .list__item a");
+
+  const options = {
+    root: null,
+    threshold: 0.5,
+  };
+
+  const observer = new IntersectionObserver(function (entries, observer) {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        const activeSectionId = entry.target.getAttribute("id");
+
+        navLinks.forEach((link) => {
+          link.parentElement.classList.remove("active");
+        });
+
+        const activeLink = document.querySelector(
+          `.nav__list .list__item a[href="#${activeSectionId}"]`
+        );
+        if (activeLink) {
+          activeLink.parentElement.classList.add("active");
+        }
+      }
+    });
+  }, options);
+
+  sections.forEach((section) => {
+    observer.observe(section);
+  });
+});
